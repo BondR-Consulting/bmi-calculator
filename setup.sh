9e7454b89ed278ac53bfb547ba0e951f43c5ca77
Simon <sm@bondr.net>
2023-06-18 11:12:45 +0300

    Adding setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Navigate to the BackEnd directory
+cd BackEnd
+echo "Installing BackEnd dependencies..."
+npm install
+
+# Navigate back to the script's directory
+cd ..
+
+# Navigate to the Database directory
+cd Database
+echo "Installing Database dependencies..."
+npm install
+
+# Navigate back to the script's directory
+cd ..
+
+# Navigate to the FrontEnd directory
+cd FrontEnd
+echo "Installing FrontEnd dependencies..."
+npm install
+
+# Navigate back to the script's directory
+cd ..
