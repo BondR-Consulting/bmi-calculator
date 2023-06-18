254be3b139275fc2d6a45e4d4220d7cb221f4bbd
Simon <sm@bondr.net>
2023-06-18 11:56:09 +0300

    Add mocking for Axios methods

--- FrontEnd/src/components/App/App.test.js
+++ FrontEnd/src/components/App/App.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
+jest.mock('axios', () => ({
+  get: jest.fn(),
+  post: jest.fn()
+}));
+
 describe('App Component', () => {
   let wrapper;
   beforeEach(() => {
